7067aee2d51b92bb8e54b422d0d65c12207fa284
marelas <marela.as@gmail.com>
2018-10-01 09:00:03 -0300

    util read

--- src/main/java/io/pivotal/camelboot/processor/FirstRequestProcessor.java
+++ src/main/java/io/pivotal/camelboot/processor/FirstRequestProcessor.java
@@ -34,3 +34,12 @@ public class FirstRequestProcessor implements Processor {
         LOG.debug("Nber of CSV records received by the csv bean : " + count);
     }
 }
+
+
+/**
+ * https://developers.redhat.com/blog/2016/11/07/microservices-comparing-diy-with-apache-camel/
+ * https://github.com/redhat-helloworld-msa/api-gateway/tree/spring-boot-camel/src/main/java/com/redhat/developers/msa/api_gateway
+ *https://github.com/jbossdemocentral/coolstore-microservice/tree/stable-ocp-3.10/coolstore-gw
+ * https://microservices.io/patterns/security/access-token.html
+ * https://access.redhat.com/documentation/en-us/red_hat_jboss_fuse/6.2/html/apache_camel_component_reference/idu-http4
+ * */
